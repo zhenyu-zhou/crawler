b35b8b71eb18e2b12328cc30771e8f0703f7657e
zzy <zzy@cs.duke.edu>
2015-10-17 00:09:58 -0400

    update_20151017

---
+++ js/adAlterData3/adAlterData3-10.17.js
@@ -0,0 +1,158 @@
+try{
+	adAlterData3Callback(
+		[
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E5%25A5%25B3%25E8%25A3%2585%26ie%3Dutf-8%26navid%3D20501%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815938-2000976',
+				txt:'秋季新款女装 初秋必备',
+				img:'http://pics3.paipaiimg.com/update/20140929/pcd_153635419.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E5%25A5%25B3%25E8%25A3%2585%26ie%3Dutf-8%26navid%3D20501%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815938-2000976',
+				txt:'',
+				img:'http://pics0.paipaiimg.com/update/20140912/pcd_145002434.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E7%2594%25B7%25E8%25A3%2585%26ie%3Dutf-8%26navid%3D22001%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815939-2001056',
+				txt:'帅气小清新',
+				img:'http://pics3.paipaiimg.com/update/20140912/pcd_300-300_1_.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E5%25A5%25B3%25E8%25A3%2585%26ie%3Dutf-8%26navid%3D20501%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815938-2000976',
+				txt:'大牌风衣',
+				img:'http://pics0.paipaiimg.com/update/20140929/pcd_1531159.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E6%2589%258B%25E8%25A1%25A8%26ie%3Dutf-8%26navid%3D32215%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815942-2002720',
+				txt:'',
+				img:'http://pics0.paipaiimg.com/update/20140912/pcd_300x30005.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E5%25A5%25B3%25E9%259E%258B%26ie%3Dutf-8%26navid%3D21036%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815940-2001142',
+				txt:'初秋必备女鞋',
+				img:'http://pics2.paipaiimg.com/update/20140912/pcd_300x300-2.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E9%25A5%25B0%25E5%2593%2581%26ie%3Dutf-8%26navid%3D2001%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815943-2002717',
+				txt:'水晶饰品公主范',
+				img:'http://pics2.paipaiimg.com/update/20140912/pcd_145429213.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E5%25A5%25B3%25E8%25A3%2585%26ie%3Dutf-8%26navid%3D20501%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815938-2000976',
+				txt:'酷酷高帮靴',
+				img:'http://pics2.paipaiimg.com/update/20140912/pcd_item-540bc15f-8260f63200000000040100003d1a67f3.3.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E7%2594%25B7%25E9%259E%258B%26ie%3Dutf-8%26navid%3D34167%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815941-2001421',
+				txt:'男神范',
+				img:'http://pics1.paipaiimg.com/update/20140929/pcd_8088-2.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E5%25A5%25B3%25E8%25A3%2585%26ie%3Dutf-8%26navid%3D20501%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815938-2000976',
+				txt:'牛仔装轻松休闲',
+				img:'http://pics3.paipaiimg.com/update/20140912/pcd_145510699.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E5%25A5%25B3%25E8%25A3%2585%26ie%3Dutf-8%26navid%3D20501%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815938-2000976',
+				txt:'清新秋款外套',
+				img:'http://pics3.paipaiimg.com/update/20140929/pcd_153136676.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E5%25A5%25B3%25E8%25A3%2585%26ie%3Dutf-8%26navid%3D20501%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815938-2000976',
+				txt:'新品连衣裙',
+				img:'http://pics1.paipaiimg.com/update/20140929/pcd_153207670.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E5%25A5%25B3%25E8%25A3%2585%26ie%3Dutf-8%26navid%3D20501%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815938-2000976',
+				txt:'气质OL',
+				img:'http://pics2.paipaiimg.com/update/20140929/pcd_153226805.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E5%25A5%25B3%25E8%25A3%2585%26ie%3Dutf-8%26navid%3D20501%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815938-2000976',
+				txt:'蕾丝晚礼服',
+				img:'http://pics1.paipaiimg.com/update/20140929/pcd_153243793.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E5%25A5%25B3%25E8%25A3%2585%26ie%3Dutf-8%26navid%3D20501%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815938-2000976',
+				txt:'内敛气质女人',
+				img:'http://pics0.paipaiimg.com/update/20140929/pcd_153314416.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E5%25A5%25B3%25E8%25A3%2585%26ie%3Dutf-8%26navid%3D20501%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815938-2000976',
+				txt:'红色秀场款风衣',
+				img:'http://pics1.paipaiimg.com/update/20140929/pcd_15333597.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E5%25A5%25B3%25E8%25A3%2585%26ie%3Dutf-8%26navid%3D20501%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815938-2000976',
+				txt:'工作生活一套就够',
+				img:'http://pics3.paipaiimg.com/update/20140929/pcd_153406835.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E5%25A5%25B3%25E8%25A3%2585%26ie%3Dutf-8%26navid%3D20501%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815938-2000976',
+				txt:'牛仔休闲裙',
+				img:'http://pics0.paipaiimg.com/update/20140929/pcd_153434747.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E5%25A5%25B3%25E8%25A3%2585%26ie%3Dutf-8%26navid%3D20501%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815938-2000976',
+				txt:'高贵红色显身材',
+				img:'http://pics0.paipaiimg.com/update/20140929/pcd_153511880.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E5%25A5%25B3%25E8%25A3%2585%26ie%3Dutf-8%26navid%3D20501%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815938-2000976',
+				txt:'完美女人都选它',
+				img:'http://pics2.paipaiimg.com/update/20140929/pcd_153540200.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E5%25A5%25B3%25E8%25A3%2585%26ie%3Dutf-8%26navid%3D20501%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815938-2000976',
+				txt:'迷人两件套',
+				img:'http://pics2.paipaiimg.com/update/20140929/pcd_153653717.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E5%25A5%25B3%25E8%25A3%2585%26ie%3Dutf-8%26navid%3D20501%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815938-2000976',
+				txt:'秋装新演绎',
+				img:'http://pics2.paipaiimg.com/update/20140929/pcd_153710416.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E5%25A5%25B3%25E8%25A3%2585%26ie%3Dutf-8%26navid%3D20501%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815938-2000976',
+				txt:'俏皮姑娘',
+				img:'http://pics3.paipaiimg.com/update/20140929/pcd_153803375.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E5%25A5%25B3%25E8%25A3%2585%26ie%3Dutf-8%26navid%3D20501%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815938-2000976',
+				txt:'大码不挑身材',
+				img:'http://pics3.paipaiimg.com/update/20140929/pcd_153838786.jpg'
+			},
+			 
+			{
+				rl:'http://re.paipai.com/tws/etgcl/click?fu=http%3A%2F%2Fexpress.paipai.com%2Fportal%2Fexp_portal_main.shtml%3Fkeyword%3D%25E7%2594%25B7%25E8%25A3%2585%26ie%3Dutf-8%26navid%3D22001%23nofare%3D0%3Bcod%3D0&pps=etg.12815935_103881_3_12815939-2001056',
+				txt:'',
+				img:'http://pics1.paipaiimg.com/update/20140929/pcd_160849368.jpg'
+			},
+			 null
+		]
+	);
+}catch(e){
+	console.log(e);
+}/*  |xGv00|9d1c1355656b05f226525a8d55d3191c */
