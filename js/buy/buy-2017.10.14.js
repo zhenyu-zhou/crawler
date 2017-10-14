a4f70cbd3d724977d4418cc6d24de4aba2656cfa
zzy <zzy@cs.duke.edu>
2017-10-14 13:43:04 -0400

    update_20171014

---
+++ js/buy/buy-2017.10.14.js
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta content="initial-scale=1, minimum-scale=1, width=device-width" name="viewport">
+    <title>
+      Temporarily unavailable – Google Wallet
+    </title>
+    <link href="//www.google.com/css/go-inlay.css" rel="stylesheet">
+    <script src="//www.google.com/js/google.js">
+</script>
+    <script>
+new gweb.analytics.AutoTrack({
+          
+          profile: 'UA-12481063-1'
+        });
+    </script>
+    <meta content="noindex" name="robots">
+  </head>
+  <body class="compact">
+    <h1>
+      <a href="/"><img alt="Google" src="//www.google.com/images/logos/google_logo_41.png"></a>
+    </h1>
+    <p>
+      Google Wallet is temporarily unavailable at this time. Please check back again in a moment.
+    </p>
+    <div id="about">
+      <a href="/">Google</a> · <a href="https://www.google.com/wallet/">Wallet Home</a> · <a href=
+      "http://www.google.com/support/wallet/">Help</a> · <a href=
+      "http://www.google.com/privacy/">Privacy Policy</a>
+    </div>
+  </body>
+</html>
