0a7cde397006c8e28d6f8874148d3c1b19b4be02
zzy <zzy@cs.duke.edu>
2019-02-15 11:07:09 -0500

    update_20190215

---
+++ html/Weibo/login-2019.2.15.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="initial-scale=1,minimum-scale=1" />
+<meta content="随时随地发现新鲜事！微博带你欣赏世界上每一个精彩瞬间，了解每一个幕后故事。分享你想表达的，让全世界都能听到你的心声！" name="description" />
+<link rel="mask-icon" sizes="any" href="//img.t.sinajs.cn/t6/style/images/apple/wbfont.svg" color="black" />
+<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+<script type="text/javascript">
+try{document.execCommand("BackgroundImageCache", false, true);}catch(e){}
+</script>
+<title>微博-随时随地发现新鲜事</title>
+<link href="//img.t.sinajs.cn/t6/style/css/module/base/frame.css?version=ae9a9ec008078a68" type="text/css" rel="stylesheet" charset="utf-8" />
+<link href="//img.t.sinajs.cn/t6/style/css/pages/growth/login_v5.css?version=ae9a9ec008078a68" type="text/css" rel="stylesheet" charset="utf-8">
+<link href="//img.t.sinajs.cn/t6/skin/default/skin.css?version=ae9a9ec008078a68" type="text/css" rel="stylesheet" id="skin_style" />
+<script type="text/javascript">
+var $CONFIG = {};
+$CONFIG['islogin'] = '0';
+$CONFIG['version'] = 'ae9a9ec008078a68';
+$CONFIG['timeDiff'] = (new Date() - 1550246569000);
+$CONFIG['lang'] = 'zh-cn';
+$CONFIG['jsPath'] = '//js.t.sinajs.cn/t5/';
+$CONFIG['cssPath'] = '//img.t.sinajs.cn/t5/';
+$CONFIG['imgPath'] = '//img.t.sinajs.cn/t5/';
+$CONFIG['servertime'] = 1550246569;
+$CONFIG['location']='login';
+$CONFIG['bigpipe']='false';
+$CONFIG['bpType']='login';
+$CONFIG['mJsPath'] = ['//js{n}.t.sinajs.cn/t5/', 1, 2];
+$CONFIG['mCssPath'] = ['//img{n}.t.sinajs.cn/t5/', 1, 2];
+$CONFIG['redirect'] = '';
+$CONFIG['vid']='';
+</script>
+<style>#js_style_css_module_global_WB_outframe{height:42px;}</style>
+</head>
+
+<body >
+<div class="B_unlog">
+
+	<div class="WB_miniblog">
+		<div class="WB_miniblog_fb">
+			<div id="weibo_top_public"></div>
+			<script src="//js.t.sinajs.cn/t6/home/js/pl/top/topInit.js?version=ae9a9ec008078a68"></script>
+			<script type="text/javascript">
+			WBtopGlobal && WBtopGlobal.init({
+			    any: "&wvr=8e443bffec86a702",
+			    islogin: "0",
+			    lang: "zh-cn",
+			    notLinkCss: true
+			});
+			</script>
+			<div class="WB_main clearfix"  id="plc_frame">
+				<div class="WB_frame">
+				<!-- 左导 -->
+		          <div class="WB_main_l">
+		          	<div id="pl_unlogin_home_leftnav"></div>
+		          </div>
+          		<!-- ／左导 -->
+		          <div id="plc_main">
+		          	<div id="plc_unlogin_home_main"></div>
+		          </div>
+				</div>
+			<div class="WB_footer S_bg2">
+        <div class="footer_link clearfix">
+           <dl class="list">
+            <dt>微博精彩</dt>
+            <dd><a class="col1 S_txt2" href="http://hot.plaza.weibo.com/?bottomnav=1&wvr=6&type=re&act=day">热门微博</a><a class="col1 S_txt2" href="http://huati.weibo.com/?bottomnav=1&wvr=6">热门话题</a></dd>
+            <dd><a class="col1 S_txt2" href="http://verified.weibo.com/?bottomnav=1&wvr=6">名人堂</a><a class="col1 S_txt2" href="http://vip.weibo.com/home?bottomnav=1&wvr=6">微博会员</a></dd>
+            <dd><a class="col1 S_txt2" href="http://photo.weibo.com/?bottomnav=1&wvr=6">微相册</a><a class="col1 S_txt2" href="http://game.weibo.com/?bottomnav=1&wvr=6">微游戏</a></dd>
+            <dd><a class="col1 S_txt2" href="http://data.weibo.com/index/?bottomnav=1&wvr=6">微指数</a></dd>
+          </dl>
+          <dl class="list">
+            <dt>手机玩微博</dt>
+            <dd><span class="T_code col2">
+                    <im
