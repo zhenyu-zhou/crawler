0de71e35f67a7c8574765405bead16d249d9c76c
zzy <zzy@cs.duke.edu>
2019-12-03 13:08:03 -0500

    update_20191203

---
+++ js/8575/8575-2019.12.3.js
@@ -0,0 +1,48 @@
+BAP.copyJSON({
+  "data": {
+    "icon_position": "top-right",
+    "default_icon": "_us",
+    "mobile_in_app_url": "",
+    "default_footer": "Privacy Controls by Ghostery, Inc.",
+    "icon_display": "normal",
+    "icon_grayscale": 30,
+    "container_opacity": 70,
+    "offset_x": 0,
+    "offset_y": 0,
+    "generic_icon": false,
+    "icon_delay": 0,
+    "nid": 8575,
+    "nwid": null,
+    "aid": 321,
+    "icid": null,
+    "skip_L2": false,
+    "behavioral": "single",
+    "generic_text": null,
+    "adv_name": "OwnerIQ",
+    "adv_msg": "OwnerIQ Privacy Policy",
+    "adv_logo": "http://c.betrad.com/a/ui/logos/321/e4dcd325193d4b60bb1904beede41dd7.jpg",
+    "adv_link": "http://www.owneriq.com/privacy-notice",
+    "mobile_message": "Tap to edit advertising preferences",
+    "display_mobile_overlay": false,
+    "mobile_advertiser_logo_url": "",
+    "default_icon_text": "AdChoices",
+    "default_generic1": "This ad has been matched to your interests. It was selected for you based on your browsing activity.",
+    "default_generic2": "This ad may have been matched to your interests based on your browsing activity.",
+    "default_generic3": "helped",
+    "default_generic4": "determine that you might be interested in an ad like this.",
+    "default_generic5": "select this ad for you.",
+    "default_generic6": "selected this ad for you.",
+    "default_link1": "More information & opt-out options",
+    "default_link2": "What is interest based advertising",
+    "default_link3": "Learn about your choices",
+    "ecid": null,
+    "hide_wi": false,
+    "hide_cl": false,
+    "server": [
+      {
+        "name": "OwnerIQ"
+      }
+    ],
+    "message_properties": {}
+  }
+});
