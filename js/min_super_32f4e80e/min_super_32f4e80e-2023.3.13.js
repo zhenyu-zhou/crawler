da4e64aad8edb535d495bb350aff1492a44f0147
zzy <zzy@cs.duke.edu>
2023-03-13 09:03:46 -0700

    update_20230313

---
+++ js/min_super_32f4e80e/min_super_32f4e80e-2023.3.13.js
@@ -0,0 +1,538 @@
+<!DOCTYPE html>
+<!--STATUS OK-->
+<html>
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    <meta content="always" name="referrer">
+    <script src="https://ss1.bdstatic.com/5eN1bjq8AAUYm2zgoY3K/r/www/nocache/imgdata/seErrorRec.js"></script>
+    <title>页面不存在_百度搜索</title>
+    <style data-for="result">
+        body {color: #333; background: #fff; padding: 0; margin: 0; position: relative; min-width: 700px; font-family: Arial, 'Microsoft YaHei'; font-size: 12px }
+        p, form, ol, ul, li, dl, dt, dd, h3 {margin: 0; padding: 0; list-style: none }
+        input {padding-top: 0; padding-bottom: 0; -moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box } img {border: none; }
+        .logo {width: 117px; height: 38px; cursor: pointer }
+         #wrapper {_zoom: 1 }
+        #head {padding-left: 35px; margin-bottom: 20px; width: 900px }
+        .fm {clear: both; position: relative; z-index: 297 }
+        .btn, #more {font-size: 14px } 
+        .s_btn {width: 95px; height: 32px; padding-top: 2px\9; font-size: 14px; padding: 0; background-color: #ddd; background-position: 0 -48px; border: 0; cursor: pointer }
+        .s_btn_h {background-position: -240px -48px }
+        .s_btn_wr {width: 97px; height: 34px; display: inline-block; background-position: -120px -48px; *position: relative; z-index: 0; vertical-align: top }
+        #foot {}
+        #foot span {color: #666 }
+        .s_ipt_wr {height: 32px }
+        .s_form:after, .s_tab:after {content: "."; display: block; height: 0; clear: both; visibility: hidden }
+        .s_form {zoom: 1; height: 55px; padding: 0 0 0 10px }
+        #result_logo {float: left; margin: 7px 0 0 }
+        #result_logo img {width: 101px }
+        #head {padding: 0; margin: 0; width: 100%; position: absolute; z-index: 301; min-width: 1000px; background: #fff; border-bottom: 1px solid #ebebeb; position: fixed; _position: absolute; -webkit-transform: translateZ(0) }
+        #head .head_wrapper {_width: 1000px }
+        #head.s_down {box-shadow: 0 0 5px #888 }
+        .fm {clear: none; float: left; margin: 11px 0 0 10px }
+        #s_tab {background: #f8f8f8; line-height: 36px; height: 38px; padding: 55px 0 0 121px; float: none; zoom: 1 }
+        #s_tab a, #s_tab b {display: inline-block; text-decoration: none; text-align: center; color: #666; font-size: 14px }
+        #s_tab b {border-bottom: 2px solid #4E6EF2; color: #222 }
+        #s_tab a:hover {color: #323232 }
+        #content_left {width: 540px; padding-left: 149px;padding-top: 2px;}
+        .to_tieba, .to_zhidao_bottom {margin: 10px 0 0 121px }
+        #help {background: #f5f6f5; zoom: 1; padding: 0 0 0 50px; float: right }
+        #help a {color: #9195a3; padding-right: 21px; text-decoration: none }
+        #help a:hover {color: #333 }
+        #foot {position: fixed; bottom:0; width: 100%; background: #f5f6f5; border-top: 1px solid #ebebeb; text-align: left; height: 42px; line-height: 42px; margin-top: 40px; *margin-top: 0; _position:absolute; _bottom:auto; _top:expression(eval(document.documentElement.scrollTop+document.documentElement.clientHeight-this.offsetHeight-(parseInt(this.currentStyle.marginTop,10)||0)-(parseInt(this.currentStyle.marginBottom,10)||0))); }
+
+        .content_none {padding: 45px 0 25px 121px } .s_ipt_wr.bg,
+        .s_btn_wr.bg, #su.bg {background-image: none }
+        .s_ipt_wr.bg {background: 0 }
+        .s_btn_wr {width: auto; height: auto; border-bottom: 1px solid transparent; *border-bottom: 0 }
+        .s_btn {width: 100px; height: 34px; color: white; letter-spacing: 1px; background: #3385ff; border-bottom: 1px solid #2d78f4; outline: medium; *border-bottom: 0; -webkit-appearance: none; -webkit-border-radius: 0 }
+        .s_btn:hover {background: #317ef3; border-bottom: 1px solid #2868c8; *border-bottom: 0; box-shadow: 1px 1px 1px #ccc }
+        .s_btn:active {background: #3075dc; box-shadow: inset 1px 1px 3px #2964bb; -webkit-box-shadow: inset 1px 1px 3px #2964bb; -moz-box-shadow: inset 1px 1px 3px #2964bb; -o-box-shadow: inset 1px 1px 3px #2964bb }
+        #lg {display: none }
+        #head .headBlock {margin: -5px 0 6px 121px }
+        #content_left .leftBlock {margin-bottom: 14px; padding-bottom: 5px; border-bottom: 1px solid #f3f3f3 }
+        .s_ipt_wr {border: 1px solid #b6b6b6; border-color: #7b7b7b #b6b6b6 #b6b6b6 #7b7b7b; background: #fff; display: inline-block; vertical-align: top; width: 592px; margin-right: 0; border-right-width: 0; border-color: #b8b8b8 transparent #ccc #b8b8b8; overflow: hidden }
+        .s_ipt_wr.ip_short {width: 439px; }
+        .s_ipt_wr:hover, .s_ipt_wr.ipthover {border-color: #999 transparent #b3b3b3 #999 }
+        .s_ipt_wr.iptfocus {border-color: #4e6ef2 transparent #4e6ef2 #4e6ef2 }
+        .s_ipt_tip {color: #aaa; position: absolute; z-index: -10; font: 16px/22px arial; height: 32px; line-height: 32px; padding-left: 7px; overflow: hidden; width: 526px }
+        .s_ipt {width: 526px; height: 22px; font: 16px/18px arial; line-height: 22px\9; margin: 6px 0 0 7px; padding: 0; background: transparent; border: 0; outline: 0; -webkit-appearance: none }
+        #kw {position: relative;display: inline-block;}
+        input::-ms-clear {display: none }
+        /*Error page css*/
+        .norsSuggest {display: inline-block; color: #333; font-family: arial; font-size: 13px; position: relative; } 
+        .norsTitle {font-size: 22px;font-weight: normal; color: #333; margin: 29px 0 25px 0; }
+        .norsTitle2 {font-family: arial; font-size: 13px; color: #666; }
+        .norsSuggest ol {margin-left: 47px; }
+        .norsSuggest li {margin: 13px 0; }
+        #content_right {
+    border-left: 1px solid #e1e1e1;
+    width: 384px;
+    margin-top: 20px;
+    float: right;
+    padding-left: 17px;
+}
+#wrapper_wrapper {
+    width: 1235px;
+    margin-top: 50px;
+}
+.cr-content {
+    width: 351px;
+    font-size: 13px;
+    line-height: 1.54em;
+    color: #333;
+    margin-top: 8px;
+    margin-left: 19px;
+    word-wrap: break-word;
+    word-break: normal;
+}
+@media screen and (max-width: 1217px) {
+    #wrapper_wrapper {
+        width: 1002px;
+    }
+    #wrapper_wrapper #content_right {
+        width: 271px;
+    }
+    #wrapper_wrapper .cr-content {
+        width: 259px;
+    }
+}
+.opr-toplist-title {
+    position: relative;
+    font-size: 14px;
+    line-height: 1.29em;
+    font-weight: 700;
+    margin-bottom: 3px;
+    font: 14px/22px Arial,sans-serif;
+    color: #222;
+    font-weight: 400;
+}
+.opr-toplist-table {
+    width: 100%;
+    border-collapse: collapse;
+    border-spacing: 0;
+    font-size: 13px;
+}
+.opr-toplist-table th,td {
+    line-height: 1.54;
+    border-bottom: 1px solid #f3f3f3;
+    text-align: left;
+}
+.opr-toplist-table thead th {
+    padding-top: 4px;
+    padding-bottom: 4px;
+    font-weight: 400;
+    color: #666;
+    white-space: nowrap;
+    background-color: #fafafa;  
+}
+.opr-toplist-table .opr-toplist-right {
+    text-align: right;
+    white-space: nowrap;
+}
+.opr-toplist-table td {
+    width: 100%;
+    font-size: 13px;
+    padding-top: 5.5px;
+    padding-bottom: 5.5px;
+    vertical-align: top;
+}
+.opr-toplist-table a:hover {
+    color: #315EFB;
+    text-decoration: underline;
+}
+.opr-toplist-table td{
+    padding-top: 5px 0;
+    border: none!important;
+    height: 20px;
+    line-height: 20px!important;
+}
+.opr-item-text{
+    font: 14px/22px Arial,sans-serif;
+    color: #2440B3;
+}
+.opr-index-item {
+    display: inline-block;
+    padding:1px 0;
+    color: #9195A3;
+    width: 20px;
+    height: 16px;
+    line-height: 100%;
+    font-size: 15px;
+    margin-right: 1px;
+    text-align: left;
+}
+.opr-index-hot1 {
+    color: #f54545;
+}
+
+.opr-index-hot2 {
+    color: #ff8547;
+}
+.opr-index-hot3 {
+    color: #ffac38;
+}
+.opr-item-text {
+    text-decoration: none;  
+}
+.opr-toplist-info {
+    color: #666;
+    text-align: right;
+    margin-top: 5px;
+}
+.opr-toplist-info>a {
+    color: #666;
+}
+/* 新加 */
+@font-face {
+  font-family: 'cIconfont';
+  src: url('./font/iconfont.eot');
+  src: url('./font/iconfont.eot?#iefix') format('embedded-opentype'),
+    url('./font/iconfont.woff2') format('woff2'),
+    url('./font/iconfont.woff') format('woff'),
+    url('./font/iconfont.ttf') format('truetype'),
+    url('./font/iconfont.svg#iconfont') format('svg');
+}
+.s_form {
+    zoom: 1;
+    padding: 15px 0 4px 16px;
+    height: 40px;
+    font-size: 0;
+}
+#result_logo{
+    margin-top: 2px;
+}
+#result_logo img {
+    width: 101px;
+    height: 33px;
+}
+.fm {
+    margin-left: 19px;
+}
+.s_ipt_wr{
+    box-sizing: border-box;
+    height:40px;
+    line-height: 40px;
+    border: 2px solid #c4c7ce;
+    border-radius: 10px 0 0 10px;
+    border-right: 0;
+    overflow: visible;
+}
+.s_btn_wr .s_btn{
+    cursor: pointer;
+    width: 112px;
+    height: 40px;
+    line-height: 41px;
+    line-height: 40px\9;
+    background-color: #4e6ef2;
+    border-radius: 0 10px 10px 0;
+    font-size: 17px;
+    box-shadow: none;
+    font-weight: 400;
+    border: 0;
+    outline: 0;
+    letter-spacing: normal;
+}
+.s_btn_wr .s_btn:hover{
+    background: #4662d9;
+}
+.s_ipt_wr.ip_short{
+    width: 480px;
+}
+#kw{
+    width: 465px;
+    height: 38px;
+    font: 16px/18px arial;
+    padding: 10px 0 10px 14px;
+    margin: 0;
+    background: transparent;
+    border: 0;
+}
+.fm{
+    margin: 0 0 15px 16px;
+    clear: none;
+    float: left;
+    position: relative;
+}
+#head{
+    border-bottom: none;
+    position: relative;
+}
+#u {
+    height: 40px;
+    line-height: 40px;
+    padding-right: 30px;
+    z-index: 301;
+    display: inline-block;
+    float: right;
+    color: #999;
+}
+#u .toindex{
+    font-family: Arial, 'Microsoft YaHei';
+    font-size: 13px;
+    color: #222222;
+    text-align: right;
+    text-decoration: none;
+}
+#u .toindex:hover{
+    color: #2640b3;
+}
+.s-tab-item:hover::before{
+    color: #222 !important;
+}
+#s_tab {
+    position: absolute;
+    left: 0;
+    background: none;
+    line-height: 36px;
+    height: 38px;
+    padding: 2px 0 0 150px;
+    float: none;
+    zoom: 1;
+    color: #626675;
+    overflow: hidden;
+}
+#s_tab a, #s_tab b {
+    display: inline-block;
+    margin-right: 24px;
+    min-width: 44px;
+    text-decoration: none;
+    font-size: 14px;
+    text-align: left;
+    line-height: 28px;
+    color: #222222;
+    font-weight: normal;
+}
+#s_tab a{
+    color: #626675;
+}
+#s_tab .s-tab-item:before {
+    display: inline-block;
+    margin-right: 2px;
+    width: 14px;
+    font-family: 'cIconfont'!important;
+    font-style: normal;
+    -webkit-font-smoothing: antialiased;
+    background: initial;
+    color: #c0c2c8;
+}
+#s_tab .cur-tab:before {
+    font-family: 'cIconfont'!important;
+    color: #626675;
+    margin-right: 2px;
+    content: '\e608';
+}
+#s_tab .s-tab-news:before {
+    content: '\e606';
+}
+#s_tab .s-tab-video:before {
+    content: '\e604';
+}
+#s_tab .s-tab-pic:before {
+    content: '\e607';
+}
+#s_tab .s-tab-zhidao:before {
+    content: '\e633';
+}
+#s_tab .s-tab-wenku:before {
+    content: '\e605';
+}
+#s_tab .s-tab-tieba:before {
+    content: '\e609';
+}
+#s_tab .s-tab-map:before {
+    content: '\e630';
+}
+#s_tab .s-tab-b2b:before {
+    content: '\e603';
+}
+#content_right{
+    border-left: none;
+}
+.norsTitle{
+    font-family: Arial, 'Microsoft YaHei';
+    font-size: 18px;
+    color: #222222;
+    text-align: justify;
+    line-height: 18px;
+}
+.norsTitle{
+    margin-bottom: 28px;
+}
+.norsTitle2{
+    font-family: Arial, 'Microsoft YaHei';
+    font-size: 14px;
+    color: #333333;
+    text-align: justify;
+    line-height: 14px;
+    margin-bottom: 12px;
+}
+.norsSuggest a{
+    color: #2440b3;
+    text-decoration: none;
+}
+.norsSuggest a:hover{
+    color: #315efb;
+    text-decoration: underline;
+}
+#foot{
+    background: #f5f5f6;
+    color: #9195a3;
+    border-top: none;
+}
+#help{
+    float:left;
+    padding-left:150px
+}
+    </style>
+</head>
+
+<body link="#0000cc">
+    <div id="wrapper" class="wrapper_l">
+        <div id="head">
+            <div class="head_wrapper">
+                <div class="s_form">
+                    <div class="s_form_wrapper">
+                        <a href="//www.baidu.com/" id="result_logo"><img src="//www.baidu.com/img/flexible/logo/pc/result@2.png" alt="到百度首页" title="到百度首页"></a>
+                        <form id="form" name="f" action="//www.baidu.com/s" class="fm">
+                            <input type="hidden" name="ie" value="utf-8">
+                            <input type="hidden" name="f" value="8">
+                            <input type="hidden" name="rsv_bp" value="1">
+                            <input type="hidden" name="ch" value="">
+                            <input type="hidden" name="tn" value="baiduerr">
+                            <input type="hidden" name="bar" value="">
+                            <span class="bg s_ipt_wr iptfocus">
+                                <input id="kw" name="wd" class="s_ipt" value="" maxlength="255" autocomplete="off" autofocus>
+                            </span><span class="bg s_btn_wr">
+                                <input type="submit" id="su" value="百度一下" class="bg s_btn">
+                            </span>
+                        </form>
+                    </div>
+                    <div id="u">
+                        <a class="toindex" href="//www.baidu.com/">百度首页</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <div class="s_tab" id="s_tab">
+        <b class="cur-tab">网页</b>
+        <a href="//www.baidu.com/?tn=news" class="s-tab-item s-tab-news" wdfield="kw">资讯</a>
+        <a href="//v.baidu.com/" class="s-tab-item s-tab-video">视频</a>
+        <a href="//image.baidu.com/" class="s-tab-item s-tab-pic">图片</a>
+        <a href="//zhidao.baidu.com/" class="s-tab-item s-tab-zhidao">知道</a>
+        <a href="//wenku.baidu.com/" class="s-tab-item s-tab-wenku">文库</a>
+        <a href="//tieba.baidu.com/index.html" class="s-tab-item s-tab-tieba">贴吧</a>
+        <a href="//map.baidu.com/" class="s-tab-item s-tab-map">地图</a>
+        <a href="//b2b.baidu.com/s?fr=wwwt" class="s-tab-item s-tab-b2b">采购</a>
+        <a href="//www.baidu.com/more/" class="s-tab-item s-tab-more">更多</a>
+    </div>
+    <div id="wrapper_wrapper">
+        <div id="content_left">
+            <div class="nors">
+                <div class="norsSuggest">
+                    <h3 class="norsTitle">很抱歉，您要访问的页面不存在！</h3>
+                    <p class="norsTitle2">温馨提示 :</p>
+                    <p class="norsTitle2">请检查您访问的网址是否正确</p>
+                    <p class="norsTitle2">如果您不能确认访问的网址，请浏览<a href="//www.baidu.com/more/index.html">百度更多</a>页面查看更多网址。</p>
+                    <p class="norsTitle2">回到顶部重新发起搜索</p>
+                    <p class="norsTitle2">如有任何意见或建议，请及时<a href="http://qingting.baidu.com/index">反馈给我们</a>。</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="foot">
+        <span id="help">
+            <a href="http://help.baidu.com/question" target="_blank">帮助</a>
+            <a href="http://www.baidu.com/search/jubao.html" target="_blank">举报</a>
+            <a href="http://jianyi.baidu.com" target="_blank">用户反馈</a>
+        </span>
+    </div>
+</body>
+<script>
+(function () {
+        var LOGURL = 'https://sp1.baidu.com/5b1ZeDe5KgQFm2e88IuM_a/cm.gif';
+        var params = 'type=wwwerror&terminal=www';
+        var img = new Image();
+        img.src = LOGURL + '?' + params;
+    })();
+    (function () {
+        if(window.recommend && window.recommend.query && window.recommend.query.length > 0) {
+            var recommendWrapper = document.createElement('div');
+            var recommendHtml = '<div class="cr-content"><div class="opr-toplist-title">' + window.recommend.title + '</div><table class="opr-toplist-table">';
+            var queryArray = window.recommend.query;
+            var itemUrl = '';
+            for(var i = 1; i < (queryArray.length+1); i++) {
+                itemUrl = '//www.baidu.com/s?word=' + encodeURIComponent(queryArray[i-1].word) + '&sa=' + queryArray[i-1].sa + '&tn=' + queryArray[i-1].tn;
+                if (queryArray[i-1].rsv_dl) {
+                    itemUrl += "&rsv_dl=" + queryArray[i-1].rsv_dl;
+                }
+                
+                if (i < 4) {
+                    recommendHtml += '<tr><td><span class="opr-index-hot' + i + ' opr-index-item">' + i + '</span><a target="_blank" href="' + itemUrl +'" class="opr-item-text">' + queryArray[i-1].word + '</a></td></tr>';
+                } else {
+                    recommendHtml += '<tr><td><span class="opr-index-item">' + i + '</span><a target="_blank" href="' + itemUrl +'" class="opr-item-text">' + queryArray[i-1].word + '</a></td></tr>';
+                }
+            }
+            recommendHtml += '</tbody></table></div>';
+            recommendWrapper.setAttribute('id', 'content_right');
+            document.getElementById('wrapper_wrapper').insertBefore(recommendWrapper, document.getElementById('content_left'));
+            var recommend = document.getElementById('content_right');
+            recommend.innerHTML = recommendHtml;
+        }
+})();
+(function(){
+    var bds = {
+        util: {}
+    };
+    var c = document.getElementById('kw').parentNode;
+
+    bds.util.getWinWidth = function(){
+        return window.document.documentElement.clientWidth;
+    };
+
+    bds.util.setFormWidth = function(){
+        var width = bds.util.getWinWidth();
+        if(width < 1217)    {bds.util.setClass(c, 'ip_short', 'add')}
+        else                {bds.util.setClass(c, 'ip_short', 'remove')};
+    };
+
+    bds.util.setClass = function(obj, class_name, set) {
+        var ori_class = obj.className,
+            has_class_p = -1,
+            ori_class_arr = [],
+            new_class = '';
+
+        if(ori_class.length) ori_class_arr = ori_class.split(' ');
+
+        for( i in ori_class_arr) {
+            if(ori_class_arr[i] == class_name) has_class_p = i;
+        }
+
+        if( set == 'remove' && has_class_p >= 0) {
+            ori_class_arr.splice(has_class_p, 1);
+            new_class = ori_class_arr.join(' ');
+            obj.className = new_class;
+        } else if( set == 'add' && has_class_p < 0) {
+            ori_class_arr.push(class_name);
+            new_class = ori_class_arr.join(' ');
+            obj.className = new_class;
+        }
+    }
+    bds.util.setFormWidth();
+
+    if (typeof document.addEventListener != "undefined") {
+        window.addEventListener('resize', bds.util.setFormWidth, false);
+        document.getElementById('kw').addEventListener('focus', function(){bds.util.setClass(c,'iptfocus', 'add');}, false);
+        document.getElementById('kw').addEventListener('blur', function(){bds.util.setClass(c,'iptfocus', 'remove');}, false);
+    } else {
+        window.attachEvent('onresize', bds.util.setFormWidth, false);
+        document.getElementById('kw').attachEvent('onfocus', function(){bds.util.setClass(c,'iptfocus', 'add');}, false);
+        document.getElementById('kw').attachEvent('onblur', function(){bds.util.setClass(c,'iptfocus', 'remove');}, false);
+    } 
+
+})();
+
+
+</script>
+
+</html>
